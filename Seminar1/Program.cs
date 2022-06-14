f4aa1742cdd7b73d21c70e850927daa0096d62a1
konstantin <ognev.k.a@mail.ru>
2022-06-14 12:04:49 +0300

    commit num1 == num2 * num2

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,8 +1,24 @@
-﻿int num, result;
+﻿// int num, result;
 
-Console.Write("Введите натуральное число, квадрат которого хотите получить: ");
-num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите натуральное число, квадрат которого хотите получить: ");
+// num = Convert.ToInt32(Console.ReadLine());
 
-result = num * num;
+// result = num * num;
 
-Console.WriteLine("Квадрат числа " + num + " равен: " + result);
+// Console.WriteLine("Квадрат числа " + num + " равен: " + result);
+
+int num1, num2;
+
+Console.Write("Введите первое натуральное число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+	Console.WriteLine("Число " + num1 + " является квадратом числа " + num2);
+}
+else
+{
+	Console.WriteLine("Число " + num1 + " не является квадратом числа " + num2);
+}
