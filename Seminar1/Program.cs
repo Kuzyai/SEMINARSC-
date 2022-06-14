5fa68913042ec660b9c5e37c76b063f2b74bf756
konstantin <ognev.k.a@mail.ru>
2022-06-14 11:47:37 +0300

    commit num * num

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int num, result;
+
+Console.Write("Введите натуральное число, квадрат которого хотите получить: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = num * num;
+
+Console.WriteLine("Квадрат числа " + num + " равен: " + result);
