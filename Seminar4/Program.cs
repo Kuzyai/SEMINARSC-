dee45f07235f100609c85f42ea4e7dc3b74f44c6
konstantin <ognev.k.a@mail.ru>
2022-06-19 22:28:24 +0300

    add Seminar4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,45 @@
+﻿/* int SumDigit(int num)
+{
+	int sum = 0;
+	for (int i = 1; i <= num; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = SumDigit(n);
+Console.WriteLine(result); */
+
+/* int MultipleDigit(int num)
+{
+	int result = 1;
+	for (int i = 1; i <= num; i++)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = MultipleDigit(n);
+Console.WriteLine(result); */
+
+/* int AmountDigit(int num)
+{
+	int i = 0;
+	while (num > 0)
+	{
+		num /= 10;
+		i++;
+	}
+	return i;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = AmountDigit(n);
+Console.WriteLine(result); */
